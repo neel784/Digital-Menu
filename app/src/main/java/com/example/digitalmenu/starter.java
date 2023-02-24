03603d782d5aeb52dbecf8816185f95ab29e54f8
neel <neelkamdar5@gmail.com>
2023-02-25 01:27:51 +0530

    First commit

---
+++ app/src/main/java/com/example/digitalmenu/starter.java
@@ -0,0 +1,14 @@
+package com.example.digitalmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class starter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starter);
+    }
+}
